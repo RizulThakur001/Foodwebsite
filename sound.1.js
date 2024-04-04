3b91b6b0f7af44661f804b575f541bb3da4c043a
RizulThakur001 <165263468+RizulThakur001@users.noreply.github.com>
2024-04-04 16:37:32 +0530

    added

---
+++ sound.1.js
@@ -0,0 +1,16 @@
+// function sound() {  
+//     const audio = new Audio('key_press.mp3');
+//     audio.play();
+ let fg = Math.floor(Math.random() * 100 + 1)
+// }
+
+function a(){
+    let input = document.getElementById('a').value;
+    if (input != fg) {
+        document.getElementById('c').innerHTML = "try";
+    }
+    else {
+        document.getElementById('c').innerHTML = "cong";
+    }
+}
+
